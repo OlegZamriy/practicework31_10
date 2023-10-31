009e27b397ae662a16846c9687cc18531c9d2270
lysenko_v <lysenko_v@itstep.academy>
2023-10-31 18:52:54 +0200

    1

--- Header.h
+++ Header.h
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <cstdlib>  
+#include <ctime>   
+
+using namespace std;
+
+
+
+
+void fillArrayWithRandomValues(int arr[], int arrSize);
+void findMinAndMax(const int arr[], int arrSize, int& minValue, int& minIndex, int& maxValue, int& maxIndex);
+
 
--- main.cpp
+++ main.cpp
@@ -1,15 +1,56 @@
-#include<iostream>
+#include"Header.h"
 
-using namespace std;
-
-int main() 
-{
-
-
-
-
-
-
-	return 0;
-}
  
+
+void fillArrayWithRandomValues(int arr[], int arrSize) {
+    for (int i = 0; i < arrSize; i++) {
+        arr[i] = rand() % 100;  
+    }
+}
+
+void findMinAndMax(const int arr[], int arrSize, int& minValue, int& minIndex, int& maxValue, int& maxIndex) {
+    if (arrSize == 0) {
+        cerr << "The array is empty." << endl;
+        return;
+    }
+
+    minValue = arr[0];
+    minIndex = 0;
+    maxValue = arr[0];
+    maxIndex = 0;
+
+    for (int i = 1; i < arrSize; i++) {
+        if (arr[i] < minValue) {
+            minValue = arr[i];
+            minIndex = i;
+        }
+        if (arr[i] > maxValue) {
+            maxValue = arr[i];
+            maxIndex = i;
+        }
+    }
+}
+
+int main() {
+    srand(time(0));
+
+    const int arrSize = 6;
+    int array[arrSize];
+
+    fillArrayWithRandomValues(array, arrSize);
+
+    int minValue, minIndex, maxValue, maxIndex;
+
+    findMinAndMax(array, arrSize, minValue, minIndex, maxValue, maxIndex);
+
+    cout << "Array: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << array[i] << " ";
+    }
+    cout << std::endl;
+
+    cout << "Minimum: " << minValue << " (index " << minIndex << ")" << endl;
+    cout << "Maximum: " << maxValue << " (index " << maxIndex << ")" << endl;
+
+    return 0;
+}
