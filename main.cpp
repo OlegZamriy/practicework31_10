8f068756c79cbd680e186949a9748013554d4d38
lysenko_v <lysenko_v@itstep.academy>
2023-10-31 19:10:11 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,15 +1,64 @@
-#include<iostream>
-
-using namespace std;
-
-int main() 
-{
+#include"Header.h"
 
 
+template <typename T>
+void reverseArray(T arr[], int arrSize) {
+    int left = 0;
+    int right = arrSize - 1;
 
+    while (left < right) {
+        T temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
 
-
-
-	return 0;
+        left++;
+        right--;
+    }
 }
- 
+
+template <typename T>
+void fillArrayWithRandomValues(T arr[], int arrSize) {
+    for (int i = 0; i < arrSize; i++) {
+        arr[i] = static_cast<T>(rand() % 100);  
+    }
+}
+
+int main() {
+    srand(time(0));
+
+    const int arrSize = 6;
+    int intArray[arrSize];
+    double doubleArray[arrSize];
+
+    fillArrayWithRandomValues(intArray, arrSize);
+    fillArrayWithRandomValues(doubleArray, arrSize);
+
+    cout << "Initial array int: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << intArray[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Initial array double: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << doubleArray[i] << " ";
+    }
+    cout << endl;
+
+    reverseArray(intArray, arrSize);
+    reverseArray(doubleArray, arrSize);
+
+    cout << "A reordered int array: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << intArray[i] << " ";
+    }
+    cout << endl;
+
+    cout << "A reordered double array: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << doubleArray[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
